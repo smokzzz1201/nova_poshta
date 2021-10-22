cb2d1982ae4003b6b5d66da98c9c760ad6d2a407
Serhii <smokzzz1201@gmail.com>
2021-10-22 15:14:49 +0300

    first commit

---
+++ src/components/App.vue
@@ -0,0 +1,64 @@
+<template>
+   <div style="margin: 50px;">
+       <h4>Виберіть місто:</h4>
+
+       <select v-model="ot">
+           <option  v-for="city in goroda" v-bind:key="city.id" v-bind:value="city.Description">{{ city.Description}}</option>
+       </select>
+       <button v-on:click="naytiOtdel">Знайти</button>
+
+        <h4>Поштове відділення:</h4>
+       <select>
+           <option  v-for="ot in otdeli" v-bind:value="ot.Description"  v-bind:key="ot.Cityid">{{ ot.Description}}</option>
+       </select>
+   </div>
+</template>
+
+<script>
+    import Vue from 'vue'
+    import axios from 'axios'
+    import VueAxios from 'vue-axios'
+ 
+   export default {
+        data: function() {
+           return {
+           goroda:[],
+           otdeli:[],
+           ot:"Запоріжжя",
+           
+        }},
+        mounted: function(){
+            
+                axios.post("https://api.novaposhta.ua/v2.0/json/Address/getCities", {
+                        apiKey: "a296ca247f95d5c0d9755e9b97b2e6a4",
+                        modelName: "Address",
+                        calledMethod: "getCities",
+                        methodProperties: {}
+                })
+                .then((response)=>{
+                    console.log(response.data);
+                    this.goroda = response.data.data;
+                })            
+        },
+        methods:{
+            naytiOtdel: function() {
+                
+                 axios.post("https://api.novaposhta.ua/v2.0/json/Address/getWarehouses", {
+                        apiKey: "a296ca247f95d5c0d9755e9b97b2e6a4",
+                        modelName: "Address",
+                        calledMethod: "getWarehouses",
+                        methodProperties: {
+                            CityName: this.ot,
+                        }
+                })
+                .then((response)=>{
+                    console.log(response.data);
+                    this.otdeli = response.data.data;
+                })
+            }
+        
+        }
+    }
+</script>
+<style scoped>
+</style>
